ff2963bf378d1ac56507788ee377087158cbe4ff
Kuber-Pathak <kuberpathak124@gmail.com>
2024-09-26 13:41:45 +0545

    Restructured and Database Connection

---
+++ backend/src/database/connect.db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userName = process.env.USERNAME;
+const password = encodeURIComponent(process.env.PASSWORD);
+const dbName = process.env.DB_NAME;
+const host = process.env.HOST;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${userName}:${password}@${host}/${dbName}?retryWrites=true&w=majority&appName=Kuber`
+    );
+    console.log("Connection Successful..");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import connectDB from "./database/connect.db.js";
 
 //Creating an App
 const app = express();
@@ -8,6 +9,9 @@ const PORT = process.env.PORT;
 
 //To make app understand json data
 app.use(express.json());
+
+//Database Connection
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`App is listening at port ${PORT}`);
